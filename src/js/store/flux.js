62442087b1fff60a312c79e7ea67ab6bd4b83e22
Luis Guillermo <abg.lgmm@gmail.com>
2023-09-23 09:01:13 +0000

    commitLGMM1

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,21 +1,47 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			titulo: 'Hola Mundo',
+			users: [],
+			demo: [{title: "FIRST",
+							background: "white",
+							initial: "white"},
+ 						 {title: "SECOND",
+							background: "white",
+							initial: "white"}]
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
+			getUsers: async () => {
+				// const url = 'https://jsonplaceholder.typicode.com/users';
+				const url = 'https://playground.4geeks.com/apis/fake/contact/agenda/spain46';
+				const options = {
+					method : 'GET'
+				}
+				const response = await fetch(url, options);
+				if (response.ok) {
+					const data = await response.json();
+					setStore({users: data })
+				} else {
+					console.log('Error: ', response.status, response.statusText)
+				}
+			},
+			addContact: async (newUser) => {
+				const url = 'https://playground.4geeks.com/apis/fake/contact/';
+				const options = {
+					method: 'POST',
+					headers: {"Content-Type": "application/json"},
+					body: JSON.stringify(newUser)
+				}
+				const response = await fetch(url, options);
+				if (response.ok) {
+					const data = await response.json();
+					console.log(data);
+					getActions().getUsers();
+				} else {
+					console.log('Error', response.status, response.statusText)
+				}
+			},
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
@@ -25,17 +51,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 				*/
 			},
 			changeColor: (index, color) => {
-				//get the store
+				// Get the store
 				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
+				// We have to loop the entire demo array to look for the respective index and change its color
 				const demo = store.demo.map((elm, i) => {
 					if (i === index) elm.background = color;
 					return elm;
 				});
-
-				//reset the global store
+				// Reset the global store
 				setStore({ demo: demo });
 			}
 		}
