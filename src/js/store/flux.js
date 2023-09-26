2eb8dea9acd4a537799cfa308e5c1dbd6e29aafb
Luis Guillermo <abg.lgmm@gmail.com>
2023-09-26 19:46:57 +0000

    commitLGMM4

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,61 +1,79 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			titulo: 'Hola Mundo',
-			users: [],
-			demo: [{title: "FIRST",
-							background: "white",
-							initial: "white"},
- 						 {title: "SECOND",
-							background: "white",
-							initial: "white"}]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			getUsers: async () => {
-				const url = 'https://playground.4geeks.com/apis/fake/contact/agenda/spain46';
-				const options = {
-					method : 'GET'
-				}
-				const response = await fetch(url, options);
-				if (response.ok) {
-					const data = await response.json();
-					setStore({users: data })
-				} else {
-					console.log('Error: ', response.status, response.statusText)
-				}
-			},
-			addContact: async (newUser) => {
-				const url = 'https://playground.4geeks.com/apis/fake/contact/';
-				const options = {
-					method: 'POST',
-					headers: {"Content-Type": "application/json"},
-					body: JSON.stringify(newUser)
-				}
-				const response = await fetch(url, options);
-				if (response.ok) {
-					const data = await response.json();
-					console.log(data);
-					getActions().getUsers();
-				} else {
-					console.log('Error', response.status, response.statusText)
-				}
-			},
-			deleteContact: async (id) => {
-				const url = 'https://playground.4geeks.com/apis/fake/contact/' + id;
-				const options = {
-					method: 'DELETE'
-				}
-				const response = await fetch(url, options);
-				if (response.ok) {
-					const data = await response.json();
-					console.log(data);
-				} else { m
-					console.log('Error', response.status, response.statusText)
-				}
-			}
-		},
-	};
+  return {
+    store: {
+      titulo: "Hola Mundo",
+      users: [],
+      contactSelected: {},
+      demo: [
+        { title: "FIRST", background: "white", initial: "white" },
+        { title: "SECOND", background: "white", initial: "white" },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      getUsers: async () => {
+        const url =
+          "https://playground.4geeks.com/apis/fake/contact/agenda/spain46";
+        const options = {
+          method: "GET",
+        };
+        const response = await fetch(url, options);
+        if (response.ok) {
+          const data = await response.json();
+          setStore({ users: data });
+        } else {
+          console.log("Error: ", response.status, response.statusText);
+        }
+      },
+      addContact: async (newUser) => {
+        const url = "https://playground.4geeks.com/apis/fake/contact/";
+        const options = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(newUser),
+        };
+        const response = await fetch(url, options);
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          getActions().getUsers();
+        } else {
+          console.log("Error", response.status, response.statusText);
+        }
+      },
+      deleteContact: async (id) => {
+        const url = "https://playground.4geeks.com/apis/fake/contact/" + id;
+        const options = {
+          method: "DELETE",
+        };
+        const response = await fetch(url, options);
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+        } else {
+          m;
+          console.log("Error", response.status, response.statusText);
+        }
+      },
+      editContact: async (id) => {
+        const store = getStore();
+        const data = {};
+        const url = "https://playground.4geeks.com/apis/fake/contact/" + id;
+        const options = {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        };
+        const response = await fetch(url, options);
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+        } else {
+          console.log("Error", response.status, response.statusText);
+        }
+      },
+    },
+  };
 };
 
 export default getState;
